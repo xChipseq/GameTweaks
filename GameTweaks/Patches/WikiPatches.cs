d1b904be54e269ccc57883d6ea4cc6dbc974ee1a
Chipseq <108542751+xChipseq@users.noreply.github.com>
2026-05-01 18:18:49 +0200

    add wiki

---
+++ GameTweaks/Patches/WikiPatches.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using TownOfUs.Assets;
+using TownOfUs.Modules.Wiki;
+
+namespace GameTweaks.Patches;
+
+[HarmonyPatch]
+public static class WikiPatches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(IngameWikiMinigame), nameof(IngameWikiMinigame.AddNewTerms))]
+    public static void AddNewTermsPostfix(IngameWikiMinigame instance)
+    {
+        instance._activeTerms.Add(new TermWikiInfo("TweakWikiTitle", "TweakWikiTerms", TouAssets.TerminologySprite));
+    }
+}
